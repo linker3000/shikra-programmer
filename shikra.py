e5b0c5156e668544ee93b0882263d70505e37e02
Nigel Kendrick <linker3000@gmail.com>
2019-12-26 15:24:29 +0000

    Fix 'Resource busy' problem

--- shikra.py
+++ shikra.py
@@ -237,6 +237,13 @@ class Shikra():
 
     def config(self):
         # set first active
+        '''
+        Linker3000 - free the device if it has already been claimed
+        '''  
+        if self.shikra_dev.is_kernel_driver_active(0):
+            reattach = True
+            self.shikra_dev.detach_kernel_driver(0)
+
         self.shikra_dev.set_configuration()
 
         # get endpoint
